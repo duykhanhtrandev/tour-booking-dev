b2428e7b9f2dec63081b2450aea63a58a62fd57b
duykhanhtrandev <khanhduytran.dev@gmail.com>
2022-11-26 22:48:01 +0700

    modelling the tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,53 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Chuyến tham quan cần một cái tên'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Chuyến tham quan cần thời lượng']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Chuyến tham quan cần biết số lượng người']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Mức độ khó của chuyến tham quan']
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
   price: {
     type: Number,
-    required: [true, 'Chuyến tham quam cần giá']
-  }
+    required: [true, 'Chuyến tham quam cần giá tiền']
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Chuyến tham quan cần đoạn mô tả']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Chuyến tham quan cần ảnh bìa']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
