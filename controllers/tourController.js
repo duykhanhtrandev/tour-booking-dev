1eff33a90f6cf5b9fb47642dbc0fdfc0f1c327cb
duykhanhtrandev <khanhduytran.dev@gmail.com>
2022-11-27 23:26:43 +0700

    filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,11 +1,28 @@
+const { query } = require('express');
 const Tour = require('./../models/tourModel');
 
 const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // const query = Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
-      result: tours.length,
+      results: tours.length,
       data: {
         tours
       }
