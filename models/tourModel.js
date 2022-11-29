5f65b0318c6283923280baa02ed9fd9ab7cf383f
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2022-11-29 22:26:00 +0700

    virtual properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,57 +1,67 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Chuyến tham quan cần một cái tên'],
-    unique: true,
-    trim: true
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Chuyến tham quan cần một cái tên'],
+      unique: true,
+      trim: true
+    },
+    duration: {
+      type: Number,
+      required: [true, 'Chuyến tham quan cần thời lượng']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Chuyến tham quan cần biết số lượng người']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Mức độ khó của chuyến tham quan']
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'Chuyến tham quam cần giá tiền']
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Chuyến tham quan cần đoạn mô tả']
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'Chuyến tham quan cần ảnh bìa']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+    startDates: [Date]
   },
-  duration: {
-    type: Number,
-    required: [true, 'Chuyến tham quan cần thời lượng']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'Chuyến tham quan cần biết số lượng người']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'Mức độ khó của chuyến tham quan']
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'Chuyến tham quam cần giá tiền']
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'Chuyến tham quan cần đoạn mô tả']
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'Chuyến tham quan cần ảnh bìa']
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
-  startDates: [Date]
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
