48085ee9bc63d5dc3a1bf2af613730a383113019
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2022-12-01 11:11:48 +0700

    handling unhandled routes

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -26,5 +22,13 @@ app.use((req, res, next) => {
 // 3) ROUTE
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handling Unhandled Routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} in this server!`
+  });
+});
 
 module.exports = app;
