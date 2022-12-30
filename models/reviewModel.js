84a9bc4f42430403151839e06647f6bb0c7fd1a0
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2022-12-31 01:06:45 +0700

    modelling reviews: parent referencing

---
+++ models/reviewModel.js
@@ -0,0 +1,38 @@
+// review / rating /createdAt / ref to tour / ref to user
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review không được để trống!']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review phải thuộc về một chuyến tham quan.']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review phải thuộc về một người dùng.']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
