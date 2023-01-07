a5fbb7451ed0167a8506d6e32cacda0a2ec48a9e
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2023-01-08 03:23:33 +0700

    building the tour overview

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,17 @@
-const getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+const getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
-    title: 'Các chuyến tham quan'
+    title: 'Các chuyến tham quan',
+    tours
   });
-};
+});
 
 const getTour = (req, res) => {
   res.status(200).render('tour', {
