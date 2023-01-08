6653ef4171e3956d6a6fd06fd314ef8c7bf16b8b
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2023-01-09 02:39:30 +0700

    building the tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,11 +13,20 @@ const getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-const getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'Du Lịch Quảng Ninh'
+const getTour = catchAsync(async (req, res) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  // 2) Build template
+  // 3) Render template using data from 1)
+  res.status(200).render('tour', {
+    title: 'Du Lịch Quảng Ninh',
+    tour
+  });
+});
 
 module.exports = {
   getOverview,
