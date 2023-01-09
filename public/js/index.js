6e081462d7b82aab36e53c07358b484761d0fa20
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2023-01-10 02:01:47 +0700

    updating user data with our api

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,13 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // DELAGATION
 if (mapBox) {
@@ -14,13 +16,20 @@ if (mapBox) {
   displayMap(locations);
 }
 
-if (loginForm) {
+if (loginForm)
   loginForm.addEventListener('submit', e => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
   });
-}
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+  userDataForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
+  });
